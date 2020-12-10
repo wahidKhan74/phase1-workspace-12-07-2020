d4c2ad32ac22076bb66b89018481a35ecff9b6dc
waheed khan <wahid.khan74@gmail.com>
2020-12-10 15:04:09 +0000

    adding queu and map

---
+++ src/com/simplilearn/collections/queue/TestArrayDeque.java
@@ -0,0 +1,11 @@
+package com.simplilearn.collections.queue;
+
+public class TestArrayDeque {
+
+	public static void main(String[] args) {
+		// WAP for generating queue for price Collections
+		// Then iterate over the price collection Queue.
+
+	}
+
+}
